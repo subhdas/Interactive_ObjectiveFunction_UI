5d0c4e9ce0e3467df32db95e0092d07823958695
subhajit das <subhajit.design@gmail.com>
2018-09-04 23:56:46 -0400

    fixes the double rangle slider

--- static/js/ConsInteraction.js
+++ static/js/ConsInteraction.js
@@ -125,6 +125,109 @@
     $('#' + containerId).append(htmlStr);
   }
 
+  // feature -range content
+  ConsInt.contentForFeatRange = function(containerId = ""){
+    var htmlStr = "<div class = 'labelCon featWtCon'>"
+    var val = "Feature-Range"
+    // htmlStr += "<div class = 'labelitemsTitle'> Set : " + val + "</div>"
+    htmlStr += "<div givenCons = '"+stri+"' class = 'labelitemsConFeat' id = 'labelitemsConId_"+val+"' ></div><br>"; // this is the event.target
+    htmlStr += "</div>";
+    $('#' + containerId).append(htmlStr);
+
+    //place the features
+    var feat = Object.keys(Main.attrDict);
+    htmlStr = "";
+    for(var i=0;i<feat.length;i++){
+      if(feat[i] == 'id') continue;
+      htmlStr += "<div class = 'wrapRowFeatWt'><label class = 'labelFeatWt' for='"+feat[i]+"checkbox-"+i+"'>" + feat[i] + "</label>"
+      htmlStr += "<input type='checkbox' parent = '"+val+"' given = '"+feat[i]+"' name='"+feat[i]+"checkbox-"+i+"' \
+      class = 'featOpt featOpt_"+feat[i]+"' id='"+feat[i]+"checkbox-"+i+"'>";
+
+      htmlStr += "<div class = 'sliderFeat' id='slider_"+feat[i]+"' >\
+      <div  parent = '"+val+"' given = '"+feat[i]+"' id='custom-handle1" + feat[i]+"' class='ui-slider-handle custom-handle1'></div>\
+      <div  parent = '"+val+"' given = '"+feat[i]+"' id='custom-handle2" + feat[i]+"' class='ui-slider-handle custom-handle2'></div>\
+      </div>"
+      htmlStr += "</div>"
+    }
+    $('#labelitemsConId_'+val).append(htmlStr);
+    $( ".featOpt" ).checkboxradio({
+       icon: false
+     });
+
+     //slider Function
+    for(var i=0;i<feat.length;i++){
+      var ran = Main.attrDict[feat[i]]['range']
+      // console.log('range found is ', ran)
+      $( "#slider_"+feat[i] ).slider({
+        range: true,
+        min: ran[0],
+        max: ran[1],
+        values: [ ran[0], ran[1] ],
+        create: function() {
+          var handle1 = $( '#custom-handle1' + feat[i] );
+          var handle2 = $( '#custom-handle2' + feat[i] );
+
+          var val1 = $(this ).slider( "values", 0 );
+          var val2 = $(this ).slider( "values", 1 );
+          handle1.text(val1);
+          handle2.text(val2);
+          // handle2.text($( this ).slider("value"));
+        },
+        slide: function( event, ui ) {
+          // var handle = $('#custom-handle' + feat[i] );
+          var handle1 =  $(this).find('.custom-handle1');
+          var handle2 =  $(this).find('.custom-handle2');
+
+          // var handle1 = $( '#custom-handle1' + feat[i] );
+          // var handle2 = $( '#custom-handle2' + feat[i] );
+          handle1.text( ui.values[0] );
+          handle2.text( ui.values[1] );
+          // console.log(' now ui value ', ui.value, feat[i], i, handle)
+        },
+        change : function(e,ui){
+          var id = ui.handle.id;
+          // console.log(' found change ', ui);
+          var ftWt =  $("#"+id).attr('parent');
+          var nam =  $("#"+id).attr('given');
+          ConsInt.activeConstraints[ftWt]['input'][nam] = ui.values;
+        }
+      });
+    }
+    //events
+    $('.featOpt').on('click', function(e){
+      // console.log('clicked attr ', e)
+      // console.log('clicked attr ', );
+      var nam = $(this).attr('given');
+      var ftWt = $(this).attr('parent');
+      $("#slider_"+nam).toggle();
+      // console.log('checked or not ', this.checked);
+      if(this.checked){
+          var v = $("#slider_"+nam).slider("option", "value");
+          ConsInt.activeConstraints[ftWt]['input'][nam] = v;
+      }else{
+          delete ConsInt.activeConstraints[ftWt]['input'][nam];
+      }
+
+      // console.log('clicked attr ', $(e.target).attr('id'));
+    })// end of clicked
+    $('.sliderFeat').hide();
+
+    var objDict = ConsInt.activeConstraints[val]['input'];
+    for(var item in objDict){
+      var v = objDict[item];
+      console.log(' now in objdict ', v , objDict, val, item)
+      $(".featOpt_"+item).prop('checked', 'checked');
+      $(".featOpt_"+item).checkboxradio( "refresh" );
+      $("#slider_"+item).slider('value',v);
+      $("#slider_"+item).toggle();
+      $("#custom-handle"+item).text(v);
+    }
+
+     $(".labelitemsConFeat").css('height', 'auto')
+     $(".labelFeatWt").css('width', '100px')
+     $(".labelFeatWt").css('text-align', 'left')
+  }// end of feature range
+
 
   // feature -weights content
   ConsInt.contentForFeatWeights = function(containerId = ""){
@@ -207,7 +310,7 @@
      $(".labelitemsConFeat").css('height', 'auto')
      $(".labelFeatWt").css('width', '100px')
      $(".labelFeatWt").css('text-align', 'left')
-  }
+  }// end of feature weights
 
   // similarity - metric label
     ConsInt.contentForCriticalItems = function(containerId = ""){
@@ -264,6 +367,7 @@
     else if(stri == "Similarity-Metric")  ConsInt.contentForSimilarItems(containerId);
     else if(stri == "Information-Gain")  ConsInt.contentForInfoGain(containerId);
     else if(stri == "Feature-Weights")  ConsInt.contentForFeatWeights(containerId);
+    else if(stri == "Feature-Range")  ConsInt.contentForFeatRange(containerId);
     else if(stri == "Critical-Items")  ConsInt.contentForCriticalItems(containerId);
 
     //style
