264219f60e226f8251cf2daf8fd23486d52b4d73
subhajit das <subhajit.design@gmail.com>
2019-03-17 15:31:30 -0400

    CHECKED THE NON CRITICAL

--- hyperOpt_optimize.py
+++ hyperOpt_optimize.py
@@ -299,21 +299,21 @@ def find_goodModel(train, test, targetTrain, targetTest, extraInfo):
         predTrain = clf.predict(train)
         predTest = clf.predict(test)
 
-        # print ' before corss val ', train.shape, len(predTrain), len(targetTrain), len(targetTrainNew), trainNew.shape
+        print ' before corss val ', train.shape, len(predTrain), len(targetTrain), len(targetTrainNew), trainNew.shape
         # targetTrain = targetTrainNew
         cross_mean_score = cross_val_score(
             estimator=clf, X=trainNew, y=targetTrainNew, scoring='precision_macro', cv=3, n_jobs=-1).mean()
 
         critScore = critical_metrics(targetTrain, predTrain)
-        sameLabScore = samelabel_metrics(targetTrainNew, predTrain)
-        similarityScore = similar_metrics(targetTrainNew, predTrain)
+        sameLabScore = samelabel_metrics(targetTrain, predTrain)
+        similarityScore = similar_metrics(targetTrain, predTrain)
 
         # critScore = 0
         # sameLabScore = 0 #samelabel_metrics(targetTrainNew, predTrain)
         # similarityScore = 0# #similar_metrics(targetTrainNew, predTrain)
 
-        targetTrainNew2, predTrainNew2 = non_critical_metrics(
-            targetTrain, predTrain)
+        # this one makes sure in the metrics we dont use the ignored items
+        targetTrainNew2, predTrainNew2 = non_critical_metrics(targetTrain, predTrain)
         print " diff target length found ", len(targetTrainNew), len(targetTrain), train.shape
 
         # ccheck for length of new target train
@@ -327,6 +327,7 @@ def find_goodModel(train, test, targetTrain, targetTest, extraInfo):
             trainT = targetTrainNew
             predT = predTrainNew2
 
+        #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ metric calc below
         # metrics for train
         try:
             exist = metObj['Precision']
