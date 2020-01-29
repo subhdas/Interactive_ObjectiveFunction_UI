e0f261188c5b1be06e184b89b8cfffbb1444532f
subhajit das <subhajit.design@gmail.com>
2020-01-28 22:39:42 -0500

    conf setup panel

--- static/js/Conflict_res.js
+++ static/js/Conflict_res.js
@@ -4,8 +4,22 @@
 
 
         //make the UI
-        CFR.setupPanel = function(){
+        CFR.setupPanel = function(containerId = 'body'){
             console.log('starting to setup CFR')
+            let confclass = 'conflictresPanel'
+            let htmlstr = "<div class ='"+confclass+"'>"
+            htmlstr += "</div>"
+            $(containerId).append(htmlstr);
+            let pos = $('#rightPanel').position()
+            let wid = $('#rightPanel').width()
+            $('.' + confclass).css('top', pos.top)
+            $('.' + confclass).css('left', pos.left)
+            $('.' + confclass).css('width', wid)
+            $('.'+confclass).hide();
+            $('#cfrBtn').on('click', function(e){
+                e.stopPropagation();            
+                $('.' + confclass).toggle();
+            })
         }
 
 
